aa81ef6770177765ea365b1baaceb73a7c662219
Aleksey Bykhun <theisease@gmail.com>
2018-10-20 17:41:57 +0200

    change API root to prod

--- src/services/api/index.js
+++ src/services/api/index.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 const AI_ROOT = `https://ai.morejust.media`
 const AI_DEV_ROOT = `http://localhost:5000`
 
-axios.defaults.baseURL = AI_DEV_ROOT
+axios.defaults.baseURL = AI_ROOT
 
 axios.interceptors.response.use(({ data }) => {
   if (!data) return Promise.reject(`Malformed response`)
