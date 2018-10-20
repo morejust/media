8c022f0b026fa7a4aeb0801ad0420cbbccd088bb
Aleksey Bykhun <theisease@gmail.com>
2018-10-20 16:07:09 +0200

    remove Landing component, split into two

---
+++ src/components/GoRead.vue
@@ -0,0 +1,44 @@
+<template>
+
+        <v-form>
+          <v-container>
+
+            <v-layout row wrap>
+
+              <v-flex xs12>
+                <v-text-field
+                  v-model="url"
+                  label="Read any article"
+                  required
+                  solo
+                  >
+
+                  <v-btn
+                    slot="append-outer"
+                    style="top: -58px"
+                    offset-y
+                    class="blue lighten-2 mt-5"
+                    dark
+                    large
+                    :href="`#/read?url=${url}`"
+                  >
+                    Process
+                  </v-btn>
+
+              </v-text-field>
+            </v-flex>
+          </v-layout>
+        </v-container>
+      </v-form>
+
+</template>
+
+<script>
+export default {
+  name: 'GoRead',
+
+  data: ({ url }) => ({
+    url: url
+  })
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,150 @@
 <template>
-  <Landing />
+  <v-content>
+    <section>
+      <v-parallax :src="hero" height="600">
+        <v-layout
+          column
+          align-center
+          justify-center
+          class="white--text"
+        >
+          <a href="#/">
+            <img src="../assets/logo.png" alt="more just media" height="200">
+          </a>
+
+          <h1 class="white--text mb-2 display-1 text-xs-center">Reading. Reinvented.</h1>
+          <div class="subheading mb-3 text-xs-center">Powered by morejust.media</div>
+          <v-btn
+            class="blue lighten-2 mt-5"
+            dark
+            large
+            href="#/analyze"
+          >
+            Get Started
+          </v-btn>
+        </v-layout>
+      </v-parallax>
+    </section>
+
+    <section>
+      <v-layout
+        column
+        wrap
+        class="my-5"
+        align-center
+      >
+        <v-flex xs12 sm4 class="my-3">
+          <div class="text-xs-center">
+            <h2 class="headline">We humans are flawed.</h2>
+            <span class="subheading">
+              Fortunately, we humans are smart enough to fix it.
+            </span>
+          </div>
+        </v-flex>
+        <v-flex xs12>
+          <v-container grid-list-xl>
+            <v-layout row wrap align-center>
+              <v-flex xs12 md4>
+                <v-card class="elevation-0 transparent">
+                  <v-card-text class="text-xs-center">
+                    <img src="https://airbnb.design/anotherlens/img/balance.gif" />
+                  </v-card-text>
+                  <v-card-title primary-title class="layout justify-center">
+                    <div class="headline text-xs-center">Balance your bias</div>
+                  </v-card-title>
+                  <v-card-text>
+                    Cras facilisis mi vitae nunc lobortis pharetra. Nulla volutpat tincidunt ornare.
+                    Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+                    Nullam in aliquet odio. Aliquam eu est vitae tellus bibendum tincidunt. Suspendisse potenti.
+                  </v-card-text>
+                </v-card>
+              </v-flex>
+              <v-flex xs12 md4>
+                <v-card class="elevation-0 transparent">
+                  <v-card-text class="text-xs-center">
+                    <img src="https://airbnb.design/anotherlens/img/opposite.gif" />
+                  </v-card-text>
+                  <v-card-title primary-title class="layout justify-center">
+                    <div class="headline">Consider the opposite</div>
+                  </v-card-title>
+                  <v-card-text>
+                    Cras facilisis mi vitae nunc lobortis pharetra. Nulla volutpat tincidunt ornare.
+                    Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+                    Nullam in aliquet odio. Aliquam eu est vitae tellus bibendum tincidunt. Suspendisse potenti.
+                  </v-card-text>
+                </v-card>
+              </v-flex>
+              <v-flex xs12 md4>
+                <v-card class="elevation-0 transparent">
+                  <v-card-text class="text-xs-center">
+                    <img src="https://airbnb.design/anotherlens/img/growth.gif" />
+                  </v-card-text>
+                  <v-card-title primary-title class="layout justify-center">
+                    <div class="headline text-xs-center">Embrace growth</div>
+                  </v-card-title>
+                  <v-card-text>
+                    Cras facilisis mi vitae nunc lobortis pharetra. Nulla volutpat tincidunt ornare.
+                    Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+                    Nullam in aliquet odio. Aliquam eu est vitae tellus bibendum tincidunt. Suspendisse potenti.
+                  </v-card-text>
+                </v-card>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </v-flex>
+      </v-layout>
+    </section>
+
+    <section>
+      <v-parallax :src="hero" height="380">
+        <v-layout column align-center justify-center>
+          <div class="headline white--text mb-3 text-xs-center">
+            Want to learn to read again?
+          </div>
+          <em>
+            Read any article
+          </em>
+
+          <GoRead />
+
+        </v-layout>
+      </v-parallax>
+    </section>
+
+    <section>
+      <InfoCard />
+    </section>
+
+    <v-footer class="red darken-2">
+      <v-layout row wrap align-center>
+        <v-flex xs12>
+          <div class="white--text ml-3">
+            See more @ <a class="white--text" href="https://github.com/morejust">morejust</a>
+          </div>
+        </v-flex>
+      </v-layout>
+    </v-footer>
+  </v-content>
 </template>
 
 <script>
+import hero from '@/assets/hero.jpeg'
+import plane from '@/assets/plane.jpg'
+import section from '@/assets/section.jpg'
 // @ is an alias to /src
-import Landing from '@/components/Landing.vue'
+import GoRead from '@/components/GoRead.vue'
+import InfoCard from '@/components/InfoCard.vue'
 
 export default {
   name: 'home',
   components: {
-    Landing
-  }
+    GoRead,
+    InfoCard
+  },
+  data: () => ({
+    hero: hero,
+    plane: plane,
+    section: section
+  })
 }
 </script>
