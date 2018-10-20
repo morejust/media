f458b0ce4677f07e0d175947b41ea42b96ecde72
Aleksey Bykhun <theisease@gmail.com>
2018-10-20 16:07:52 +0200

    hide nav

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <v-app light>
-    <nav>
+    <!-- <nav>
       <router-link to="/">Home</router-link> |
       <router-link to="/read">Read</router-link>
-    </nav>
+    </nav> -->
     <router-view/>
   </v-app>
 </template>
