edd173f24dc23ec4555884b14cd4ae618c449bfa
Aleksey Bykhun <theisease@gmail.com>
2018-10-20 16:08:23 +0200

    /analyze (ya.ru like)

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import Analyze from './views/Analyze.vue'
 
 Vue.use(Router)
 
@@ -12,10 +13,15 @@ export default new Router({
       component: Home
     },
     {
+      path: '/analyze',
+      name: 'analyze',
+      component: Analyze
+    },
+    {
       path: '/read',
       name: 'read',
       props: ({ query }) => ({ url: query.url }),
-      component: () => import(/* webpackChunkName: "readmode" */ './views/ReadMode.vue'),
-    },
+      component: () => import(/* webpackChunkName: "readmode" */ './views/ReadMode.vue')
+    }
   ]
 })
---
+++ src/views/Analyze.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-content>
+    <section>
+      <v-parallax :src="plane" dark height="600">
+        <v-layout
+          column
+          align-center
+          justify-center
+          class="white--text"
+        >
+          <a href="#/">
+            <img src="../assets/logo.png" alt="more just media" height="200">
+          </a>
+
+          <GoRead />
+        </v-layout>
+      </v-parallax>
+    </section>
+
+    <section>
+      <InfoCard />
+    </section>
+
+  </v-content>
+</template>
+
+<script>
+import GoRead from '@/components/GoRead.vue'
+import InfoCard from '@/components/InfoCard.vue'
+
+import plane from '@/assets/plane.jpg'
+
+export default {
+  name: 'analyze',
+  components: {
+    GoRead,
+    InfoCard
+  },
+  data: () => ({ plane })
+}
+</script>
