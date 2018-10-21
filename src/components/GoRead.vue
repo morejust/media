23358474e651cbc76037a91f35bfcc6218078fd3
Aleksey Bykhun <theisease@gmail.com>
2018-10-21 04:09:44 +0200

    topic examples

--- src/components/GoRead.vue
+++ src/components/GoRead.vue
@@ -26,6 +26,18 @@
               Process
             </v-btn>
 
+            <v-btn
+              slot="append-outer"
+              style="top: -58px"
+              offset-y
+              class="lighten-2 mt-5"
+              dark
+              large
+              v-on:click="fillExample"
+            >
+              Example
+            </v-btn>
+
           </v-text-field>
         </v-flex>
       </v-layout>
@@ -54,10 +66,27 @@
 import api from '@/services/api'
 import router from '@/router'
 
+const randomElement = arr => arr[ Math.floor(Math.random() * arr.length) ]
+
+const topics = [
+  'iPhone',
+  'Donald Trump',
+  'Putin',
+  'Catalunia',
+  'HackUPC',
+  'Germany',
+  'Spain',
+  'TheOnion',
+]
+
 export default {
   name: 'GoRead',
 
   methods: {
+    fillExample: function () {
+      this.url = randomElement(topics)
+    },
+
     submitForm: async function () {
       const isURL = /^http(s)?:\/\//.test(this.url)
 
